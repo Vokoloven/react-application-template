788b7ea81a540360781ad744c7fcb3e507657033
Ruslan Volovenko <vokoloven@gmail.com>
2025-02-19 20:46:32 +0200

    feature/TFS-21230: Implement jest and react testing library config and settings

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='App'>
       <header className='App-header'>
         <img src='/react.svg' className='App-gear' alt='Gear Animation' />
-        <h1>Welcome to React Application Template v18.3.1</h1>
+        <h1 aria-label='qwerty'>Welcome to React Application Template v18.3.1</h1>
         <p>Dive into a world where technology meets creativity!</p>
         <button className='start-button' onClick={() => window.open('https://react.dev', '_blank')}>
           Explore React
