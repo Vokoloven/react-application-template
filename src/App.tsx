80e009febc0c97528f7b73bdbe231d3a3bbe76d3
Ruslan Volovenko <vokoloven@gmail.com>
2025-02-19 17:43:26 +0200

    feature/TFS-21230: Added base styles and logo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import './App.css';
+
 const App = () => {
   return (
-    <>
-      <div>React Application Template</div>
-      <div>Test</div>
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <img src='/react.svg' className='App-gear' alt='Gear Animation' />
+        <h1>Welcome to React Application Template v18.3.1</h1>
+        <p>Dive into a world where technology meets creativity!</p>
+        <button className='start-button' onClick={() => window.open('https://react.dev', '_blank')}>
+          Explore React
+        </button>
+      </header>
+    </div>
   );
 };
 
