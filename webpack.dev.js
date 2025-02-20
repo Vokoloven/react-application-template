b7c0dccc7ff238a0bc47d84115713151cddcbfea
Ruslan Volovenko <vokoloven@gmail.com>
2025-02-20 14:04:50 +0200

    feature/TFS-21230: Added possibility to use different ports in dev mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,31 @@
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
+const { detect } = require('detect-port');
 const commonConfig = require('./webpack.common');
 
-module.exports = () => {
+module.exports = async () => {
+  const defaultPort = 3000;
+  let port;
+
+  try {
+    port = await detect(defaultPort);
+    console.log(`Requested port: ${defaultPort}, Assigned port: ${port}`);
+  } catch (err) {
+    console.error('Error detecting free port:', err);
+    port = defaultPort + 1;
+    console.log(`Falling back to port: ${port}`);
+  }
+
   const devConfig = {
     mode: 'development',
     output: {
-      publicPath: 'http://localhost:3000/',
+      publicPath: `http://localhost:${port}/`,
       filename: '[name].js',
       chunkFilename: '[name].chunk.js'
     },
     devServer: {
-      port: 3000,
+      port,
       open: true,
       hot: true,
       liveReload: false,
